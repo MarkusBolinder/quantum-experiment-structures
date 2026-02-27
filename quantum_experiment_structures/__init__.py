426079d87a23aaf74c1fa349e8d16d031750273b
Markus Bolinder <142605752+MarkusBolinder@users.noreply.github.com>
2026-02-27 11:51:06 +0100

    Add import modules

---
+++ quantum_experiment_structures/__init__.py
@@ -0,0 +1,9 @@
+__all__ = ["generator", "data", "causal_contextuality_scenario"]
+
+# import submodules
+from . import generator  # noqa: F401
+from . import causal_contextuality_scenario  # noqa: F401
+from .causal_contextuality_scenario import CausalContextualityScenario  # noqa: F401
+
+# subpackage
+from . import data  # noqa: F401
---
+++ quantum_experiment_structures/data/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["schemas"]
+
+from . import schemas  # noqa: F401
