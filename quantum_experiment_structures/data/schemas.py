4abfc596601e1fcfda1b6800677aab76c1f7314e
Markus Bolinder <142605752+MarkusBolinder@users.noreply.github.com>
2026-03-04 11:03:36 +0100

    Update CCS schema more with uniqueItems

--- quantum_experiment_structures/data/schemas.py
+++ quantum_experiment_structures/data/schemas.py
@@ -14,7 +14,26 @@ CCS_SCHEMA = {
                     "properties": {"m": {"type": "string"}, "v": {"type": "integer"}},
                     "additionalProperties": False,
                 },
+                "uniqueItems": True,
+                "minItems": 1,
             },
+            "uniqueItems": True,
+        },
+        "contexts": {
+            "type": "array",
+            "items": {
+                "type": "array",
+                "items": {"type": "string"},
+                "uniqueItems": True,
+                "minItems": 1,
+            },
+            "uniqueItems": True,
+        },
+        "cover": {
+            "allOf": [
+                {"$ref": "#/$defs/contexts"},
+                {"minItems": 1},  # force the cover to have at least one context
+            ]
         },
     },
     "required": ["ms", "c"],
@@ -31,27 +50,22 @@ CCS_SCHEMA = {
                         "type": "array",
                         "items": {
                             "type": "object",
-                            "required": ["v", "l"],
+                            "required": ["v"],  # the leaf property can be calculated later
                             "properties": {"v": {"type": "integer"}, "l": {"type": "boolean"}},
                             "additionalProperties": False,
                         },
+                        "uniqueItems": True,
                         "minItems": 1,
                     },
                     # no 'minItems'-requirement here because it can be calculated from the rest
-                    "c": {
-                        "type": "array",
-                        "items": {"type": "array", "items": {"type": "string"}},
-                    },
+                    "c": {"$ref": "#/$defs/contexts"},
                 },
                 "additionalProperties": False,
             },
+            "uniqueItems": True,
             "minItems": 1,
         },
-        "c": {
-            "type": "array",
-            "items": {"type": "array", "items": {"type": "string"}},
-            "minItems": 1,
-        },
+        "c": {"$ref": "#/$defs/cover"},
         "n": {"type": "array", "items": {"type": "string"}},
         "h": {
             "type": "object",
