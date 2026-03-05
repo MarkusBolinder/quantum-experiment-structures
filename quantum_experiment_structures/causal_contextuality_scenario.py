a41877aa4cb524a8d25b921386c1a1ab1cc106c9
Markus Bolinder <142605752+MarkusBolinder@users.noreply.github.com>
2026-03-05 09:51:01 +0100

    Improve CCS-class
    Added more methods to check correctness of the causal contextuality
    scenario, deleted a method that check uniqueness of the cover/contexts
    as this is now handled by the schema. Furthermore, added some aggregate
    methods to run all checks or all adds, or to completely validate, add,
    and then check the CCS. Also implemented the method to append to a JSON
    Lines file and fixed some bugs in the existing code.

--- quantum_experiment_structures/causal_contextuality_scenario.py
+++ quantum_experiment_structures/causal_contextuality_scenario.py
@@ -1,6 +1,7 @@
 """Validate a JSON representation of a CCS using both a schema and additional consistency checks."""
 
 from collections import defaultdict
+import inspect
 import json
 from pathlib import Path
 
@@ -22,8 +23,9 @@ class CausalContextualityScenario:
             json_data: a dict-object representing well-formed JSON.
         """
         self.data = json_data
+        self.measurements = set(measurement["m"] for measurement in self.data["ms"])
 
-    def validate_data(self):
+    def validate(self):
         """Validate the data using the CCS schema."""
         try:
             jsonschema.validate(self.data, schema=CCS_SCHEMA)
@@ -32,41 +34,61 @@ class CausalContextualityScenario:
             print(e)
             return False
 
-    def check_consistency(self, allow_duplicates=False):
-        """Ensure that the enabling relations are consistent.
+    def check_consistency(self):
+        """Ensure that the enabling relations are consistent and free of duplicates.
 
         Assume X is a measurement setting with outcomes 0 or 1, then an enabling relation is
         consistent if it does not contain conflicting events for any measurement, meaning the set of
         enabling relations does not contain {(X,0),(X,1)} or similar.
 
-        Args:
-            allow_duplicates: if True, an enabling relation of the form {(X,0),(X,0)} is not treated
-                as a consistency error; if False, then it is.
-        Returns:
-            a boolean indicating if the scenario is consistent (True if yes).
+        Raises:
+            ValueError if there is any inconsistency or duplicate evens in an enabling relation.
         """
         for measurement in self.data["ms"]:
             for enabling_relation in measurement["e"]:
-                required = set()
+                seen = set()
                 for event in enabling_relation:
-                    if not allow_duplicates:
-                        event_id = [event["m"]]
-                    else:
-                        event_id = [event["m"], tuple(event.values())]
-                    # necessary condition: the measurement (event_id[0]) is in the set
-                    # sufficient condition:
-                    # 1) if no duplicates, then the above is sufficient OR
-                    # 2) if duplicates are allowed, then there is a consistency error if the event
-                    #    tuple (event_id[1]) differs, i.e. if the outcome value is different.
-                    if event_id[0] in required and (
-                        not allow_duplicates or event_id[1] not in required
-                    ):
-                        return False
-                    required.update(event_id)
+                    measurement = event["m"]
+                    if measurement in seen:
+                        raise ValueError(
+                            f"Duplicate measurement {measurement} "
+                            f"in enabling relation {enabling_relation}"
+                        )
+                    seen.add(measurement)
         return True
 
-    def calculate_leafs(self):
-        """Iterate through the scenario and determine which measurement outcomes are leaves."""
+    def add_leaves(self):
+        """Add the leaf field to all measurement outcomes in the CCS.
+
+        Note that this will overwrite any existing leaf-fields.
+        """
+        self._handle_leaves(True)
+
+    def check_leaves(self):
+        """Check all measurement outcomes' leaves for correctness; otherwise raise error.
+
+        Raises:
+            ValueError: if any measurement outcome does not have the leaf field, or if the outcome
+                is incorrectly labeled as leaf/not a leaf.
+        """
+        if not self._handle_leaves(False):
+            raise ValueError(
+                "Leaves are not valid. Either a leaf is missing, or a leaf has the incorrect value."
+            )
+        return True
+
+    def _handle_leaves(self, check):
+        """Iterate through the scenario and determine which measurement outcomes are leaves.
+
+        Args:
+            check: if False, then all measurement outcomes will be populated with the leaf
+                field, but an existing leaf will be left as is; if True, then all measurement
+                outcomes' leaves are checked that they both exist and are correctly annotated as a
+                leaf or not.
+
+        Returns:
+            boolean indicating if the leaves are correct or that the missing leaves were written.
+        """
         # put all events that are part of an enabling relation in a hash set
         enabling_events = set(
             tuple(event.values())
@@ -74,15 +96,20 @@ class CausalContextualityScenario:
             for enabling_relation in measurement["e"]
             for event in enabling_relation
         )
-        # iterate through all measurement outcomes and mark leaves accordingly
+        # iterate through all measurement outcomes check/write leaves accordingly
         for measurement in self.data["ms"]:
             measurement_variable = measurement["m"]
             for outcome in measurement["o"]:
-                if (measurement_variable, outcome["v"]) not in enabling_events:
-                    outcome["l"] = True
-                else:
-                    # NOTE: might be redundant, but cost is probably negligible
-                    outcome["l"] = False
+                leaf = outcome.get("l")
+                if leaf is None:
+                    if check:
+                        return False
+                    outcome["l"] = (measurement_variable, outcome["v"]) not in enabling_events
+                elif check:
+                    correct = (measurement_variable, outcome["v"]) not in enabling_events
+                    if leaf != correct:
+                        return False
+        return True
 
     def calculate_memberships(self):
         """Calculate which contexts each measurement setting is part of.
@@ -90,13 +117,26 @@ class CausalContextualityScenario:
         Returns:
             a dict mapping each measurements to all contexts it appears in.
         """
-        measurement_to_contexts = defaultdict(list)
+        measurements_to_contexts = defaultdict(list)
         for context in self.data["c"]:
             for measurement in context:
-                # NOTE: this assumes that there are no duplicate contexts,
-                # nor measurements within the context
-                measurement_to_contexts[measurement].append(context)
-        return measurement_to_contexts
+                # NOTE: because the schema forbids duplicate contexts and duplicate measurements
+                # within a context, this is fine
+                measurements_to_contexts[measurement].append(context)
+        return measurements_to_contexts
+
+    def add_memberships(self):
+        """Add .ms.[$k].c field if it is not already there.
+
+        Which contexts each measurement is part of can be calculated based on the top-level cover,
+        so it is optional in the schema, and, if the field is missing, this method will add it.
+        Whereas if it is already there, we will simply ignore it for now, and let the later check
+        determine whether it is correct.
+        """
+        measurements_to_contexts = self.calculate_memberships()
+        for measurement in self.data["ms"]:
+            if "c" not in measurement:
+                measurement["c"] = measurements_to_contexts[measurement["m"]]
 
     def check_contexts(self):
         """Certify that the cover and the memberships are consistent and valid.
@@ -105,7 +145,6 @@ class CausalContextualityScenario:
         nested with. Furthermore, all contexts that appear as these 'membership'-contexts should
         also be present in the top level cover of the scenario.
         """
-        # TODO: combine this method with totality of cover method, or break into two parts
         cover = set(frozenset(context) for context in self.data["c"])
         contexts = set()
         for measurement in self.data["ms"]:
@@ -116,37 +155,48 @@ class CausalContextualityScenario:
                 contexts.add(context)
         return cover == contexts
 
-    def check_totality_of_union(self):
-        """Ensure that the union of all contexts covers all measurements and nothing more."""
+    def check_cover(self):
+        """Ensure union of all contexts in the cover covers all measurements and nothing more.
+
+        Because the schema validation forces the cover to have unique contexts and that the contexts
+        themselves have unique measurements, we only need to check that the union of all contexts
+        equals the set of measurements.
+        """
         # TODO: if needed, investigate if representing measurements as bit vectors can optimize
         measurements_in_contexts = set(
             measurement for context in self.data["c"] for measurement in context
         )
-        measurements = set(measurement["m"] for measurement in self.data["ms"])
         # this also returns False if the contexts contain measurements that are not in the scenario
-        return measurements_in_contexts == measurements
+        return measurements_in_contexts == self.measurements
 
-    def check_unique_contexts(self, check_all=True):
-        """Ensure that the cover does not contain duplicate contexts.
+    def check_unique_values(self):
+        """Ensure that there are no duplicates in the values of any measurement's outcomes.
 
-        Args:
-            check_all: indicates whether all the contexts for each measurement should be checked for
-            duplicates too. If False, only the top level field 'c' is checked.
+        The schema can ensure that the outcome-objects are unique, but because these objects contain
+        both a value ('v') field and a leaf ('l') field, you may still have duplicate values. E.g.
+        the following array of outcomes is valid against the schema:
+            [
+                {"v": 0},
+                {"v": 0, "l": true},
+                {"v": 0, "l": false}
+            ]
+        even though all outcome objects have the same value.
         """
-        # TODO: can frozenset be used everywhere?
-        if check_all:
-            for measurement in self.data["ms"]:
-                contexts = set(frozenset(context) for context in measurement["c"])
-                if len(contexts) != len(measurement["c"]):
-                    return False
-        contexts = set(frozenset(context) for context in self.data["c"])
-        if len(contexts) != len(self.data["c"]):
-            return False
+        for measurement in self.data["ms"]:
+            outcomes = measurement["o"]
+            values = set(outcome["v"] for outcome in outcomes)
+            if len(values) != len(outcomes):
+                return False
         return True
 
     def add_human_readable(self):
-        """Add a human readable representation of the CCS to the data."""
+        """Add a human readable representation of the CCS to the data.
+
+        There are no checks to ensure that the human readable format correctly described the CCS.
+        However, the schema enforces some contstraints on the format of the human readable part.
+        """
         # TODO: cache results that are calculated in methods, or store them as attributes
+        # document that human readable is not checked.
         measurements = "{" + ", ".join(measurement["m"] for measurement in self.data["ms"]) + "}"
         outcomes = {
             measurement["m"]: set(outcome["v"] for outcome in measurement["o"])
@@ -181,20 +231,70 @@ class CausalContextualityScenario:
             "c": cover,
         }
 
-    def to_json(self, filename):
+    # TODO: investigate how this should be handled
+    def wip_check_no_superset_contexts(self):
+        """Check that no context is a superset of another context.
+
+        In the contextuality setting, it is not meaningful to have contexts that are supersets of
+        other contexts, since you could simply always choose the larger context.
+        """
+        if len(self.data["c"]) < 2:
+            return True
+        contexts = [frozenset(context) for context in self.data["c"]]
+        for i, c1 in enumerate(contexts):
+            for c2 in contexts[i + 1 :]:
+                intersection = c1 & c2
+                if intersection == c1 or intersection == c2:
+                    return False
+        return True
+
+    def all_checks(self):
+        """Perform all checks."""
+        for name, member in inspect.getmembers(self):
+            if inspect.ismethod(member) and name.startswith("check"):
+                ok = member()
+                if not ok:
+                    raise ValueError(f"Inconsistency detected: {name} failed.")
+        return True
+
+    def all_adds(self):
+        """Add everything that can be added based on the base CCS data."""
+        for name, member in inspect.getmembers(self):
+            if inspect.ismethod(member) and name.startswith("add"):
+                member()
+
+    def to_json(self, filename, indent=None):
         """Flush data to a JSON file.
 
         Args:
             filename: path-like name of the output file.
         """
         path = Path(filename)
-        with path.open() as f:
-            json.dump(f)
+        with path.open("w") as f:
+            json.dump(self.data, f, indent=indent)
 
     def append_to_json_lines(self, filename):
-        """Append the CCS to a JSON lines file specified by filename.
+        """Append the CCS data to a JSON Lines file.
+
+        Creates the file if it does not already exist. Each call writes
+        one JSON object on a single line.
 
         Args:
-            filename: name of the file to which the data shall be appended.
+            filename: path-like name of the .jsonl file.
         """
-        pass
+        path = Path(filename)
+
+        with path.open("a") as f:
+            json.dump(self.data, f)
+            f.write("\n")
+
+    def everything(self):
+        # first validate against schema
+        if not self.validate():
+            raise jsonschema.ValidationError()
+        # then add missing fields
+        self.all_adds()
+        # then check that everything is correct
+        self.all_checks()
+        # TODO: validate against schema again?
+        return True
