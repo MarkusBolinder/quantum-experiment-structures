89a833518d7388584d71c2d187d585785d10960f
Markus Bolinder <142605752+MarkusBolinder@users.noreply.github.com>
2026-03-17 08:52:23 +0100

    Improve cluster generation script using dataframes
    Given that we have a schema for the input objects and for the output CCS
    objects, it is natural to utilize Spark dataframes instead of the RDDs.
    This means refactoring quite a bit, but in essence, we validate the
    input settings, calculate the number of scenarios each partition should
    generate, extract the seed from the settings. Then the settings are
    broadcasted as an immutable object to all nodes in the cluster. The main
    generation logic is then done by instantiating a separate CCSGenerator
    object for each partition and giving it a deterministic, but unique,
    seed. After the dataframe has been transformed to contain the CCSs, we
    save the results to Parquet files.

--- quantum_experiment_structures/utils/cluster_ccs_generator.py
+++ quantum_experiment_structures/utils/cluster_ccs_generator.py
@@ -3,38 +3,24 @@
 
 import copy
 import json
+from pathlib import Path
 import uuid
 
+import pandas as pd
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType
 
 import quantum_experiment_structures as qes
 from quantum_experiment_structures.data.schemas import CCS_GENERATOR_SETTINGS_SCHEMA
 from quantum_experiment_structures.utils import utils
 
 
-def _generate_partition(partition, settings_broadcast):
-    """Run generation inside a Spark partition."""
-    settings = settings_broadcast.value
-
-    for i in partition:
-        obj = copy.deepcopy(settings)
-
-        seed = obj.get("seed")
-        if seed is not None:
-            obj["seed"] = seed + i
-
-        generator = qes.CCSGenerator(**obj)
-
-        for ccs in generator.generate():
-            yield json.dumps(ccs.data)
-
-
-def run_generation(settings, n_scenarios, output_dir=None, partitions=None):
+def run_generation(n_scenarios, settings, output_dir=None, partitions=None):
     """Run distributed generation using Spark.
 
     Args:
-        settings: Input object adhering to CCS_GENERATOR_SETTINGS_SCHEMA.
         n_scenarios: Number of independent generation runs.
+        settings: Input object adhering to CCS_GENERATOR_SETTINGS_SCHEMA.
         output_dir: Output directory (Spark will create partition files).
         partitions: Number of Spark partitions.
 
@@ -50,7 +36,7 @@ def run_generation(settings, n_scenarios, output_dir=None, partitions=None):
 
     settings = copy.deepcopy(settings)
     validator.validate(settings)
-    # Spark will handle output, so, make sure that output_dir is None
+    # Spark will handle output, so make sure that output_dir is None
     settings["output_dir"] = None
 
     if output_dir is None:
@@ -59,17 +45,61 @@ def run_generation(settings, n_scenarios, output_dir=None, partitions=None):
     if partitions is None:
         partitions = min(sc.defaultParallelism, n_scenarios)
 
-    # Broadcast validated settings to workers
+    seed = settings.pop("seed")
+    del settings["n_scenarios"]
+    # broadcast validated settings and seed to workers
     settings_bc = sc.broadcast(settings)
+    seed_bc = sc.broadcast(seed)
 
-    # Parallelize scenario indices
-    seeds_rdd = sc.parallelize(range(n_scenarios), partitions)
+    # calculate the workload for each input object
+    n_scenarios_per_shard = n_scenarios // partitions
+    n_pads = n_scenarios - n_scenarios_per_shard * partitions
+    n_scenarios_col = [n_scenarios_per_shard] * partitions
+    # distribute remainder to first n_pads shards
+    for i in range(n_pads):
+        n_scenarios_col[i] += 1
+    rows = list(zip(range(partitions), n_scenarios_col))
 
-    # Run distributed generation
-    results_rdd = seeds_rdd.mapPartitions(lambda part: _generate_partition(part, settings_bc))
+    # create df with input describing number of scenarios for each partition
+    df = spark.createDataFrame(sc.parallelize(rows, partitions), ["shard_id", "n_scenarios"])
+    # FIXME: handle the Spark schema in a better way -- should it also support 'n' (notes)?
+    with Path("quantum_experiment_structures/data/spark_ccs_schema.json").open("r") as f:
+        spark_schema = StructType.fromJson(json.load(f))
 
-    # Write distributed JSONL output
-    results_rdd.saveAsTextFile(output_dir)
+    def generator_df_wrapper(df_iterator):
+        """Generate partition-level data, executed on Spark workers."""
+        settings = settings_bc.value
+        orig_seed = seed_bc.value
+        column_names = [field.name for field in spark_schema.fields]
+
+        flush_threshold = 10_000
+        buffer = []
+
+        for df in df_iterator:
+            for shard_id, n_scenarios in df[["shard_id", "n_scenarios"]].itertuples(index=False):
+                local_seed = orig_seed + int(shard_id)
+                generator = qes.CCSGenerator(seed=local_seed, n_scenarios=n_scenarios, **settings)
+
+                for ccs in generator.generate():
+                    buffer.append(ccs.data)
+
+                    if len(buffer) >= flush_threshold:
+                        yield pd.DataFrame.from_records(buffer, columns=column_names)
+                        buffer.clear()
+
+        if buffer:
+            yield pd.DataFrame.from_records(buffer, columns=column_names)
+
+    # distributed generation
+    result_df = df.mapInPandas(generator_df_wrapper, schema=spark_schema)
+
+    # write output
+    result_df.write.mode("overwrite").parquet(output_dir)
+    # NOTE: from the parquet data, you can get back to the JSON form by:
+    # 1) df = pd.read_parquet(path)
+    # 2) row = df.iloc[i, :]  # some row (could also do df.iterrows())
+    # 3) json_data = json.loads(row.to_json())
+    # 4) ccs = qes.CausalContextualityScenario(json_data)
 
     return output_dir
 
@@ -81,7 +111,7 @@ def main():
     settings = {"seed": 0}
 
     output_dir = run_generation(
-        settings=settings, output_dir="output", n_scenarios=100, partitions=8
+        n_scenarios=1000, settings=settings, output_dir="output", partitions=8
     )
 
     print("Output written to:", output_dir)
