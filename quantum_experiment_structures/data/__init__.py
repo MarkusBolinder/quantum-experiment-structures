d4dac214759a52d9121edcbc0b6569d7f3cf1e76
Markus Bolinder <142605752+MarkusBolinder@users.noreply.github.com>
2026-05-13 13:21:38 +0200

    Add the Dedekind numbers as an integer sequence
    Added a data file for integer sequences and populated it with the
    Dedekind numbers. These numbers relate to the number of possible causal
    contextuality scenarios.
    
    https://oeis.org/A000372

--- quantum_experiment_structures/data/__init__.py
+++ quantum_experiment_structures/data/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["local_covers", "schemas"]
+__all__ = ["integer_sequences", "local_covers", "schemas"]
 
+from . import integer_sequences  # noqa: F401
 from . import local_covers  # noqa: F401
 from . import schemas  # noqa: F401
---
+++ quantum_experiment_structures/data/integer_sequences.py
@@ -0,0 +1,16 @@
+"""Static storage of integer sequences."""
+
+# https://oeis.org/A000372
+# NOTE: these are all the known values as of 2026 (entry 10 was calculated in 2023)
+DEDEKIND_NUMBERS = [
+    2,
+    3,
+    6,
+    20,
+    168,
+    7581,
+    7828354,
+    2414682040998,
+    56130437228687557907788,
+    286386577668298411128469151667598498812366,
+]
